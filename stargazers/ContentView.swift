f904854b9f363fc228046fc98f0e63b953222c8b
Fabio M. Sim <sim.fabio.fms@gmail.com>
2021-07-22 15:09:35 +0700

    Initial Commit

---
+++ stargazers/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  stargazers
+//
+//  Created by Fabio Sim on 22/07/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ stargazers/stargazersApp.swift
@@ -0,0 +1,17 @@
+//
+//  stargazersApp.swift
+//  stargazers
+//
+//  Created by Fabio Sim on 22/07/21.
+//
+
+import SwiftUI
+
+@main
+struct stargazersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
